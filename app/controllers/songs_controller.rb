12a7c220a80a025ec89c2be1249a44e92380f404
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-13 20:12:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,19 @@
 class SongsController < ApplicationController
+
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      @songs = Artist.find(params[:artist_id]).songs
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
