8a095e01b2cce41dc78c16a236736c7b4ff6be01
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-13 21:03:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,9 +10,6 @@ class SongsController < ApplicationController
 
   def show
       @song = Song.find(params[:id])
-      if @song.nil?
-        redirect_to artist_songs_path(@artist)
-      end
   end
 
   def new
